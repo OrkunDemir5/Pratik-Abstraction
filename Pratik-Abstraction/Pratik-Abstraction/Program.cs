c3efa70c940a7cb09c81e54825008f0577294622
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-28 00:29:06 +0300

    Pratik-Abstraction

---
+++ Pratik-Abstraction/Pratik-Abstraction/Program.cs
@@ -0,0 +1,11 @@
+﻿// Çalışanlardan birkaç örnek nesne oluşturulması
+using Pratik_Abstraction;
+
+YazilimGelistirici yazilimci = new YazilimGelistirici { Ad = "Ali", Soyad = "Yılmaz", Departman = "Yazılım" };
+ProjeYoneticisi projeYoneticisi = new ProjeYoneticisi { Ad = "Hasan", Soyad = "Çıldırmış", Departman = "Proje Yönetimi" };
+SatisTemsilcisi satisci = new SatisTemsilcisi { Ad = "Ayşe", Soyad = "Kara", Departman = "Satış" };
+
+// Her çalışanın görevini çağırma
+yazilimci.Gorev();
+projeYoneticisi.Gorev();
+satisci.Gorev();
---
+++ Pratik-Abstraction/Pratik-Abstraction/calisanlar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Abstraction
+{
+    // Abstract Calisan sınıfı
+    abstract class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+
+        // Abstract metod tanımı (her pozisyon bu metodu kendi göreviyle dolduracak)
+        public abstract void Gorev();
+    }
+
+    // YazilimGelistirici sınıfı, Calisan'dan miras alır
+    class YazilimGelistirici : Calisan
+    {
+        // Yazılım geliştirici için görev
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}, Yazılım Geliştirici olarak çalışıyorum.");
+        }
+    }
+
+    // ProjeYoneticisi sınıfı, Calisan'dan miras alır
+    class ProjeYoneticisi : Calisan
+    {
+        // Proje yöneticisi için görev
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}, Proje yöneticisi olarak çalışıyorum.");
+        }
+    }
+
+    // SatisTemsilcisi sınıfı, Calisan'dan miras alır
+    class SatisTemsilcisi : Calisan
+    {
+        // Satış temsilcisi için görev
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad}, Satış temsilcisi olarak çalışıyorum.");
+        }
+    }
+}
